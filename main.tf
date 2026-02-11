9cb613ae47e13a18683ae787467ead40f775fd8c
JuanPalma1407 <juan.palma@pacific-hs.com>
2026-02-11 17:09:59 -0500

    Add Terraform configuration files for AWS deployment and S3 backend setup

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.0"
+  
+  backend "s3" {
+    bucket  = "fhir-terraform-state-920373013005"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
